32df9145bf9f2802def92c7b65aad6aef88b223b
Benjamin Fleischer <github@benjaminfleischer.com>
2017-07-06 16:17:04 -0500

    Get swagger-ui version from package.json

--- lib/swagger_ui_engine/version.rb
+++ lib/swagger_ui_engine/version.rb
@@ -1,4 +1,13 @@
 module SwaggerUiEngine
   VERSION = '1.0.2'.freeze
-  SWAGGER_UI_VERSION = '2.2.10'.freeze
+  def self.swagger_ui_version
+    @swagger_ui_version ||=
+      begin
+        require 'json'
+        lib = File.expand_path('..', __FILE__)
+        root = File.expand_path File.join(lib, '..', '..')
+        file = File.join(root, 'package.json')
+        JSON.parse(File.read(file)).fetch("dependencies").fetch("swagger-ui")
+      end
+  end
 end
